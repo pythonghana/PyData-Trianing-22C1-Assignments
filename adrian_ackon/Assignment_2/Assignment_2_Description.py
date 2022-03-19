3c30983bd6f37e22c70f48e77b13a684b881633c
akackon <adrianackon@rocketmail.com>
2022-03-19 07:28:03 +0000

    adrian ackon: replace lost files from merge

---
+++ adrian_ackon/Assignment_2/Assignment_2_Description.py
@@ -0,0 +1,6 @@
+'''
+Use beautiful and request, lxml, html_rser to scrape that url
+Put your jupyter notebook and the csv file on Github using Git from the command line
+not GUI
+https://www.jumia.com.gh/phones-tablets
+'''
